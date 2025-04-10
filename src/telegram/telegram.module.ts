6ec229aac6615de781b5e5daee8e182a3eefccb1
Teslyuk Yurii <yurickv@gmail.com>
2025-04-10 11:53:44 +0300

    update telegram.module

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,19 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { TelegramService } from './telegram.service';
-// import { TelegramController } from './telegram.controller';
-// import { ConfigModule } from '@nestjs/config';
-// import { OpenAIService } from 'src/openai/openai.service';
-// import { UserModule } from 'src/user/user.module';
-
-// @Module({
-//     imports: [ConfigModule.forRoot(), UserModule],
-//     providers: [TelegramService, OpenAIService],
-//     controllers: [TelegramController],
-//     exports: [TelegramService],
-// })
-
-// export class TelegramModule {}
-
 import { Module } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
 import { ConfigModule } from '@nestjs/config';
@@ -23,17 +7,19 @@ import { FoodPreferenceService } from './food/food-preference.service';
 import { FoodInputService } from './food/food-input.service';
 import { OnboardingService } from './onboarding/onboarding.service';
 import { ConfirmationService } from './confirmation/confirmation.service';
+import { TelegramController } from './telegram.controller';
 
 @Module({
     imports: [ConfigModule, UserModule],
     providers: [
-        TelegramService,
-        FoodPreferenceService,
-        FoodInputService,
         OpenAIService,
+        TelegramService,
+        FoodInputService,
         OnboardingService,
         ConfirmationService,
+        FoodPreferenceService,
     ],
+    controllers: [TelegramController],
     exports: [TelegramService],
 })
 export class TelegramModule {}
