75dedfa72e151e9e711feaeaf5d23810908b3e0a
Teslyuk Yurii <yurickv@gmail.com>
2025-06-09 21:23:18 +0300

    small fix crs plitic

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    });
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
