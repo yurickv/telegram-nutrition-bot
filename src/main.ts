9330dc59eaded5823631de2dce0e99073b27a309
Teslyuk Yurii <yurickv@gmail.com>
2025-06-10 11:36:33 +0300

    update Cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,22 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const allowedOrigins = ['http://localhost:5173', 'https://admin-page-nutri-day.vercel.app/'];
+
     app.enableCors({
-        origin: 'http://localhost:5173',
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
         credentials: true,
     });
+    // app.enableCors({
+    //     origin: 'http://localhost:5173',
+    //     credentials: true,
+    // });
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
